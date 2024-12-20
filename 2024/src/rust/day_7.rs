d6c5e0924d3d1306b87d540f96fd61cdb10ef90e
Aiden Jeffrey <aiden.jeffrey@loroco.tech>
2024-12-20 12:13:38 +0000

    day 7 wip

--- 2024/src/rust/day_7.rs
+++ 2024/src/rust/day_7.rs
@@ -1,13 +1,22 @@
-use std::fmt::Display;
+use std::slice::Iter;
 
 use std::env;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 enum Operation {
-  MUL,
-  ADD,
+  Mul = 0,
+  Add = 1,
+  Cnt = 2,
 }
+
+// impl Operation {
+//   pub fn all() -> Iter<'static, Operation> {
+//       static OPERS: [Operation; 2] = [Operation::Mul, Operation::Add];
+//       OPERS.iter()
+//   }
+// }
+
 
 #[derive(Debug, PartialEq, Eq)]
 struct Line {
@@ -24,14 +33,26 @@ fn main() -> io::Result<()> {
 
   for line in lines {
     let line = line.replace(":", "");
-    let tokens: Vec<_> = line.split(" ")
+    let values: Vec<_> = line.split(" ")
       .collect::<Vec<_>>().iter()
       .map(|c| c.parse::<u32>().unwrap()).collect();
 
-    let answer = tokens[0];
-    // build operations
-    for i in 2..tokens.len() {
+    let mut operations: Vec<Operation> = Vec::new();
+    let answer = values[0];
+    // build operations list
+    let oper_cnt = Operation::Cnt as u32;
+    let num_opers = (values.len() - 1) as u32;
+    let max = u32::pow(oper_cnt, num_opers);
 
+    println!("{} - {}", max, line);
+
+    for n in 0..max {
+      let mut n = n;
+      for _ in 0..num_opers {
+        n /= oper_cnt;
+        print!("{}", n % oper_cnt);
+      }
+      println!();
     }
 
   }
